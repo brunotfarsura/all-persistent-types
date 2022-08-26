f321545c80dfb6a710d57b0f1ee9f3ae7bc10cd5
Bruno Telles Farsura <bruno.telles.farsura@accenture.com>
2022-08-25 21:02:57 -0300

    initial screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,43 @@
+import 'package:all_persistent_types/utils/customWidgets.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  final Text title = const Text("Flutter Persistência");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: title
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: buildSvgIcon("images/sqlite-icon.svg"),
+            title: const Text("SQLite"),
+            subtitle: const Text("Lista de pessoas"),
+            trailing: const Icon(Icons.navigate_next),
+            //onTap: ,
+          ),
+          divisorListMain(),
+          ListTile(
+            leading: buildSvgIcon("images/db.svg"),
+            title: const Text("Floor"),
+            subtitle: const Text("Lista de livros"),
+            trailing: const Icon(Icons.navigate_next),
+            //onTap: ,
+          ),
+          divisorListMain(),
+          ListTile(
+            leading: buildSvgIcon("images/firebase-icon.svg"),
+            title: const Text("Firebase"),
+            subtitle: const Text("Lista de carros"),
+            trailing: const Icon(Icons.navigate_next),
+            //onTap: ,
+          ),
+          divisorListMain()
+        ]
+      ));
+  }
+}
---
+++ lib/utils/customWidgets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+Widget buildSvgIcon(String pathName) => SvgPicture.asset(
+  pathName,
+  width: 48,
+  height: 48,);
+
+Widget divisorListMain() => const Divider(
+  height: 1,
+  color: Colors.black54,
+);
